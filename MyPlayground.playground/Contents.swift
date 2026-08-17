501737d6b4faadf748e12bc7058a33c4b9c5e1f4
rajputarun1 <rajputarunpratapsingh@gmail.com>
2026-08-17 12:32:06 +0530

    Add simple Swift program

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,35 +1,10 @@
 ﻿import Foundation
 
-print("======================================")
-print(" XCODE PLAYGROUND")
-print("======================================")
+let a = 10
+let b = 20
 
-let name = "Arun"
+let sum = a + b
 
-print("")
-print("Hello, \(name)!")
-print("")
-
-let numbers = [10, 20, 30, 40, 50]
-
-print("Numbers:")
-print(numbers)
-
-let sum = numbers.reduce(0, +)
-
-print("")
-print("Sum = \(sum)")
-
-let squares = numbers.map {
-    $0 * $0
-}
-
-print("")
-print("Squares:")
-print(squares)
-
-print("")
-print("Swift Playground is running on macOS.")
-print("Xcode/Swift environment is working.")
-print("")
-print("======================================")
+print("First number: \(a)")
+print("Second number: \(b)")
+print("Sum: \(sum)")
